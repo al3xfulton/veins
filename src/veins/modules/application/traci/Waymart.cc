85a14c468cb48832a9c79ed98d1e761fe7193bdc
fulton <al3x.fulton@gmail.com>
2018-10-31 11:13:11 -0400

    import waymart files

---
+++ src/veins/modules/application/traci/Waymart.cc
@@ -0,0 +1,86 @@
+#include "veins/modules/application/traci/Waymart.h"
+
+Define_Module(TraCIDemo11p);
+
+void Waymart11p::initialize(int stage) {
+    BaseWaveApplLayer::initialize(stage);
+    if (stage == 0) {
+        sentMessage = false;
+        lastDroveAt = simTime();
+        currentSubscribedServiceId = -1;
+    }
+}
+
+void Waymart11p::onWSA(WaveServiceAdvertisment* wsa) {
+    if (currentSubscribedServiceId == -1) {
+        mac->changeServiceChannel(wsa->getTargetChannel());
+        currentSubscribedServiceId = wsa->getPsid();
+        if  (currentOfferedServiceId != wsa->getPsid()) {
+            stopService();
+            startService((Channels::ChannelNumber) wsa->getTargetChannel(), wsa->getPsid(), "Mirrored Traffic Service");
+        }
+    }
+}
+
+void Waymart11p::onWSM(WaveShortMessage* wsm) {
+    findHost()->getDisplayString().updateWith("r=16,green");
+
+    if (mobility->getRoadId()[0] != ':') traciVehicle->changeRoute(wsm->getWsmData(), 9999);
+    if (!sentMessage) {
+        sentMessage = true;
+        //repeat the received traffic update once in 2 seconds plus some random delay
+        wsm->setSenderAddress(myId);
+        wsm->setSerial(3);
+        scheduleAt(simTime() + 2 + uniform(0.01,0.2), wsm->dup());
+    }
+}
+
+void Waymart11p::handleSelfMsg(cMessage* msg) {
+    if (WaveShortMessage* wsm = dynamic_cast<WaveShortMessage*>(msg)) {
+        //send this message on the service channel until the counter is 3 or higher.
+        //this code only runs when channel switching is enabled
+        sendDown(wsm->dup());
+        wsm->setSerial(wsm->getSerial() +1);
+        if (wsm->getSerial() >= 3) {
+            //stop service advertisements
+            stopService();
+            delete(wsm);
+        }
+        else {
+            scheduleAt(simTime()+1, wsm);
+        }
+    }
+    else {
+        BaseWaveApplLayer::handleSelfMsg(msg);
+    }
+}
+
+void Waymart11p::handlePositionUpdate(cObject* obj) {
+    BaseWaveApplLayer::handlePositionUpdate(obj);
+
+    // stopped for for at least 10s?
+    if (mobility->getSpeed() < 1) {
+        if (simTime() - lastDroveAt >= 10 && sentMessage == false) {
+            findHost()->getDisplayString().updateWith("r=16,red");
+            sentMessage = true;
+
+            WaveShortMessage* wsm = new WaveShortMessage();
+            populateWSM(wsm);
+            wsm->setWsmData(mobility->getRoadId().c_str());
+
+            //host is standing still due to crash
+            if (dataOnSch) {
+                startService(Channels::SCH2, 42, "Traffic Information Service");
+                //started service and server advertising, schedule message to self to send later
+                scheduleAt(computeAsynchronousSendingTime(1,type_SCH),wsm);
+            }
+            else {
+                //send right away on CCH, because channel switching is disabled
+                sendDown(wsm);
+            }
+        }
+    }
+    else {
+        lastDroveAt = simTime();
+    }
+}
---
+++ src/veins/modules/application/traci/Waymart.h
@@ -0,0 +1,35 @@
+#ifndef Waymart_H
+#define Waymart_H
+
+#include "veins/modules/application/ieee80211p/BaseWaveApplLayer.h"
+
+/**
+ * @brief
+ * A tutorial demo for TraCI. When the car is stopped for longer than 10 seconds
+ * it will send a message out to other cars containing the blocked road id.
+ * Receiving cars will then trigger a reroute via TraCI.
+ * When channel switching between SCH and CCH is enabled on the MAC, the message is
+ * instead send out on a service channel following a WAVE Service Advertisement
+ * on the CCH.
+ *
+ * @author Christoph Sommer : initial DemoApp
+ * @author David Eckhoff : rewriting, moving functionality to BaseWaveApplLayer, adding WSA
+ *
+ */
+
+class Waymart11p : public BaseWaveApplLayer {
+	public:
+		virtual void initialize(int stage);
+	protected:
+		simtime_t lastDroveAt;
+		bool sentMessage;
+		int currentSubscribedServiceId;
+	protected:
+        virtual void onWSM(WaveShortMessage* wsm);
+        virtual void onWSA(WaveServiceAdvertisment* wsa);
+
+        virtual void handleSelfMsg(cMessage* msg);
+		virtual void handlePositionUpdate(cObject* obj);
+};
+
+#endif
